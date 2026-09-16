d26c70775603363912b876e78a0786d1e07de285
Kizito-James <kmjames637@gmail.com>
2026-09-16 15:38:38 +0300

    divide.2

--- divide.cpp
+++ divide.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
-using namespace std;
 #include <vector>
 #include <string>
 #include <iomanip>
+
+using namespace std;
+
+// Function handles calculation only and returns a vector of rates
+vector<double> calculateHourlyRates(const vector<double>& totalPay, 
+                                   const vector<double>& totalHours) {
+    vector<double> rates(totalPay.size(), 0.0);
+
+    for (size_t i = 0; i < totalPay.size(); ++i) {
+        if (totalHours[i] != 0) {
+            rates[i] = totalPay[i] / totalHours[i];
+        }
+    }
+
+    return rates;
+}
 
 int main() {
     vector<string> employees = {"A", "B", "C", "D", "E"};
     vector<double> totalPay = {225000, 228000, 240000, 220000, 180000};
     vector<double> totalHours = {45, 38, 48, 40, 40};
-    vector<double> hourlyRate(employees.size());
 
-    
+    // Calculate rates using the function
+    vector<double> hourlyRate = calculateHourlyRates(totalPay, totalHours);
+
+    // Print output inside main
     cout << "Hourly Rate\n";
     cout << "-------------------------------------------------------------------------\n";
-    
 
-    // Perform division: Total pay / Total hours
-    for (int i = 0; i < employees.size(); ++i) {
+    for (size_t i = 0; i < employees.size(); ++i) {
         if (totalHours[i] != 0) {
-        hourlyRate[i] = totalPay[i] / totalHours[i];
-        cout  <<"Employee" <<" "<<employees[i] <<" "<<"-"<<" "
-        << hourlyRate[i] << " UGX/hour\n";
+            cout << "Employee " << employees[i] << " - " 
+                 << fixed << setprecision(2) << hourlyRate[i] << " UGX/hour\n";
         }
     }
 
